d8d0073caec4951ec86d2a53347f6c0415d1578e
Maitray-Jariwala <113186935+Maitray-Jariwala@users.noreply.github.com>
2023-11-06 12:14:34 +0530

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,1 +1,25 @@
+#ifndef MENU_H
+#define MENU_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Menu
+{
+public:
+    struct ItemDetails
+    {
+        string foodItem;
+        float itemprice;
+    };
+
+    static ItemDetails Details[10];
+
+    Menu();
+
+    void displayMenu();
+};
+
+#endif
 
